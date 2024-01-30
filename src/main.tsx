4b269507e1a270bfe2d345de2c47cd0a2ce9a79a
DanielBello7 <gokebello@gmail.com>
2024-01-30 16:38:07 -0500

    chore: Setup Application

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence } from 'framer-motion';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -5,6 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AnimatePresence mode="wait">
+      <App />
+    </AnimatePresence>
   </React.StrictMode>,
 )
