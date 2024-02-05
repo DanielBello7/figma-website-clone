8198f30e0f76c6c52d3b6bfcf666fe4a7e8cb29a
DanielBello7 <gokebello@gmail.com>
2024-02-05 15:06:41 -0500

    chore: updates

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
       script: "serve",
       env: {
         PM2_SERVE_PATH: "dist",
-        PM2_SERVE_PORT: 3000,
+        PM2_SERVE_PORT: 5500,
         PM2_SERVE_SPA: "true"
       },
     }
