5bbdc0157bd765f19e0e287e82151d94a968e806
DanielBello7 <gokebello@gmail.com>
2024-02-05 14:28:39 -0500

    chore: correction

---
+++ ecosystem.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'figma-clone',
+      exec_mode: 'cluster',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      script: "serve",
+      env: {
+        PM2_SERVE_PATH: "dist",
+        PM2_SERVE_PORT: 3000,
+        PM2_SERVE_SPA: "true"
+      },
+    }
+  ]
+}
